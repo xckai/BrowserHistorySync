9b9909ac0422273e2bf2efeb5bd1cac4e6153c57
xckai <xckai123@gmail.com>
2022-05-12 22:13:36 +0800

    feat: bing host support cn second domain name

--- Web/src/services/wallpaperService.ts
+++ Web/src/services/wallpaperService.ts
@@ -6,20 +6,29 @@ import { get, map, trim } from 'lodash';
 import { getRecommendResolution4BingWallpaper } from 'src/commonLibary/utils';
 
 export interface BingWallWrapperInfo { imgUrl: string, title: string, copyRightLink: string ,copyRight:string}
-
+function getBingHost() {
+  if (navigator.language == 'zh-CN') {
+    return "cn.bing.com";
+  }
+  if (new Date().getTimezoneOffset() == -480) {
+    return "cn.bing.com";
+  }
+  return "bing.com"
+}
+const bingHost = getBingHost();
 class WallpaperService{
   async getBingWallPaper() {
     return axios.get<any, AxiosResponse<BingWallWrapperInfo>>(
       `${window.syncManagerConfig?.dataServerUrl ?? ""}/api/HttpProxy`,
       {
         params: {
-          url: "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN"
+          url: `https://${bingHost}/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=${navigator.language}`
         }
       }
     ).then(res => {
       return {
         title: get(res.data, "images[0].title"),
-        imgUrl: `https://bing.com${get(res.data, "images[0].url")}`.replace("1920x1080",`${getRecommendResolution4BingWallpaper()}`),
+        imgUrl: `https://${bingHost}${get(res.data, "images[0].url")}`.replace("1920x1080",`${getRecommendResolution4BingWallpaper()}`),
         copyRightLink: get(res.data, "images[0].copyrightlink"),
         copyRight:get(res.data, "images[0].copyright")
       }
